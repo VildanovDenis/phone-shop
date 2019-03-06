7c62f0285c24870c42dec656f2638d9df991e793
Denis <disamapm@gmail.com>
2019-03-06 17:29:38 +0300

    Делает карточку товара и задает ей стили

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -20,7 +20,6 @@ export class ProductList extends Component {
                   })
                 }}
               </ProductConsumer>
-              >
             </div>
           </div>
         </div>
